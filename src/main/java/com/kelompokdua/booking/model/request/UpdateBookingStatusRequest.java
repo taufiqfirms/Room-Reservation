1a4fec09c6b54ea10ce2d6907abe9de260e7b5f2
Kevin Dwi Rizky <kevindwirizkyy@gmail.com>
2024-04-06 12:19:30 +0700

    create validation

--- src/main/java/com/kelompokdua/booking/model/request/UpdateBookingStatusRequest.java
+++ src/main/java/com/kelompokdua/booking/model/request/UpdateBookingStatusRequest.java
@@ -1,6 +1,7 @@
 package com.kelompokdua.booking.model.request;
 
 import com.kelompokdua.booking.constant.EBookingRoom;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data @Builder
 @AllArgsConstructor @NoArgsConstructor
 public class UpdateBookingStatusRequest {
+    @NotEmpty
     private EBookingRoom status;
     private String notes;
 }
